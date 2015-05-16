4124c434d0a773f0444f48cfa0fb37d93a074aab
Ashish Dubey <ashish.dubey91@gmail.com>
2015-05-17 00:12:11 +0530

    switch to setuptools

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
-from distutils.core import setup
+from setuptools import setup
+
 
 setup(
     name='browserstack',
     version='0.0.1',
-    packages=['browserstack',],
+    author="Ashish Dubey",
+    author_email="ashish.dubey91@gmail.com",
+    packages=['browserstack', ],
     license='MIT',
     long_description='Python wrapper for BrowserStack REST API.',
-    install_requires=['requests',],
+    install_requires=['requests', ],
 )
